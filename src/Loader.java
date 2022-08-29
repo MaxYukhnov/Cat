d00b38b0d5991fc94e45a6d7353a4029aeb66de4
MaxYukhnov <remus2733@gmail.com>
2022-08-29 18:41:19 +0300

    Init

--- src/Loader.java
+++ src/Loader.java
@@ -3,8 +3,24 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat fedya = new Cat();
+        System.out.println("Fedya weight:" + fedya.getWeight());
+        fedya.meow();
+        System.out.println("Fedya weight:" + fedya.getWeight());
 
-        System.out.println(cat.getStatus());
+        System.out.println(fedya.getStatus());
+
+        Cat vasya = new Cat();
+        System.out.println("Vasya weight:" + vasya.getWeight());
+        vasya.feed(200.8);
+        System.out.println("Vasya weight:" + vasya.getWeight());
+        System.out.println(vasya.getStatus());
+
+        Cat seroga = new Cat();
+        System.out.println("Seroga weight:" + seroga.getWeight());
+        seroga.drink(456.77);
+        System.out.println("Seroga weight:" + seroga.getWeight());
+        System.out.println(seroga.getStatus());
+
     }
 }
